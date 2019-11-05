53fa48477bb5c885e8ca57288595d624054e568a
Maxime TheSpyGeek <isnel.maxime@gmail.com>
2019-11-05 01:29:33 +0100

    creation d'un ocean

---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+
+{
+
+    public float heightOfVariation;
+    public float timeOfCycle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iTween.MoveBy(this.gameObject, iTween.Hash("y", heightOfVariation, "time", timeOfCycle, "looptype", "pingpong", "easetype", iTween.EaseType.easeInOutSine));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Readme.md
+++ Readme.md
@@ -1,4 +1,3 @@
-[![unity version](https://img.shields.io/badge/unity%20version-2018.4.2f1-green.svg)]()
 # Sea of Whales
 
 Sea of whales un jeu simulant la vie mais surtout la survie des baleines dans le monde. Vous incarnez une baleine. Votre but est simple vous devez survivre, vous reproduire et proteger votre baleineau.
