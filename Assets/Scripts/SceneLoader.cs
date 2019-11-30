e86b0ae75208d9c7b9d454f39f1f46bf343228cd
Maxime TheSpyGeek <isnel.maxime@gmail.com>
2019-11-30 17:10:40 +0100

    test menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string scene;
+
+
+    public void LoadSceneMode() {
+        SceneManager.LoadScene(scene);
+    }
+
+
+}
