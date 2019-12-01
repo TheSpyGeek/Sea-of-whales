5f511047072ea309243eba7ee1a0ceb3988abea8
Maxime TheSpyGeek <isnel.maxime@gmail.com>
2019-12-01 15:40:54 +0100

    debut du menu de pause

---
+++ Assets/Scripts/SceneLoaderWithoutSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoaderWithoutSlider : MonoBehaviour
+{
+
+
+    public void LoadScene(int indexScene) {
+        StartCoroutine(LoadSceneAsync(indexScene));
+    }
+
+
+    IEnumerator LoadSceneAsync(int indexScene) {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(indexScene);
+        
+        while(!operation.isDone) {
+            
+
+            yield return null;
+        }
+
+    }
+    
+
+}
