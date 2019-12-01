e8c9f9712ec9f233d26403fa14e93ade33b43733
Maxime TheSpyGeek <isnel.maxime@gmail.com>
2019-12-01 15:12:29 +0100

    le bouton jouer marche

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,11 +2,15 @@
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
 
+    public GameObject objSlider;
+    public Slider slider;
 
+    public Text sliderText;
 
     public void LoadScene(int indexScene) {
         StartCoroutine(LoadSceneAsync(indexScene));
@@ -16,7 +20,14 @@ public class SceneLoader : MonoBehaviour
     IEnumerator LoadSceneAsync(int indexScene) {
         AsyncOperation operation = SceneManager.LoadSceneAsync(indexScene);
 
+        objSlider.SetActive(true);
+
         while(!operation.isDone) {
+
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+            sliderText.text = progress * 100 + "%";
 
             yield return null;
         }
