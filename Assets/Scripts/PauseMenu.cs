6966894cab0818a7daffe0c10dffeb10c8d7f146
Maxime TheSpyGeek <isnel.maxime@gmail.com>
2019-12-01 15:52:54 +0100

    ajout d'un menu de paue

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(gamePaused) {
+                Reprendre();
+            } else {
+                Pause();
+            }
+        }
+        
+    }
+
+
+
+    public void Pause() {
+        pauseMenuUI.SetActive(true);
+        gamePaused = true;
+        Time.timeScale = 0f;
+    }
+
+
+    public void Reprendre() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+
+    }
+
+
+    public void LoadMenu() {
+
+    }
+
+}
