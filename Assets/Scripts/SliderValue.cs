dcb00862b00c471b2be52ab005d35fa99ef51131
prakrit974 <32086134+prakrit974@users.noreply.github.com>
2019-12-02 14:52:32 +0100

    ajout d'un slider pour la vitesse du jeu

---
+++ Assets/Scripts/SliderValue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour
+{
+
+    public Slider slider;
+
+
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "x" + slider.value;
+    }
+}
